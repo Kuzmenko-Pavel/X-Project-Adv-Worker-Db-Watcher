39eff5795fdf8a7397d958d24d27942173424451
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2018-06-18 17:28:03 +0300

    change sql structure

--- x_project_adv_worker_db_watcher/models/campaign2categories.py
+++ x_project_adv_worker_db_watcher/models/campaign2categories.py
@@ -1,4 +1,4 @@
-from sqlalchemy import (Column, Integer, BigInteger, ForeignKey, select, Index, cast)
+from sqlalchemy import (Column, BigInteger, ForeignKey, select, Index, cast)
 
 from .__libs__.sql_view import create_view
 from .meta import Base
@@ -7,7 +7,7 @@ from .meta import Base
 class Campaign2Categories(Base):
     __tablename__ = 'campaign2categories'
     id_cam = Column(BigInteger, ForeignKey('campaign.id', ondelete='CASCADE'), primary_key=True, nullable=False)
-    id_cat = Column(Integer, ForeignKey('categories.id', ondelete='CASCADE'), primary_key=True, nullable=False)
+    id_cat = Column(BigInteger, ForeignKey('categories.id', ondelete='CASCADE'), primary_key=True, nullable=False)
 
     __table_args__ = (
         {'prefixes': ['UNLOGGED']}
@@ -19,11 +19,11 @@ class MVCampaign2Categories(Base):
         Base.metadata,
         'mv_campaign2categories',
         select([
-            cast(Campaign2Categories.id_cam, BigInteger).label('id_cam_pk'),
-            cast(Campaign2Categories.id_cat, Integer).label('id_cat_pk')
+            Campaign2Categories.id_cam,
+            Campaign2Categories.id_cat
         ]).select_from(Campaign2Categories),
         is_mat=True)
 
 
-Index('ix_mv_campaign2categories_id_cam_pk_id_cat_pk',
-      MVCampaign2Categories.id_cam_pk, MVCampaign2Categories.id_cat_pk, unique=True)
+Index('ix_mv_campaign2categories__id_cam__id_cat',
+      MVCampaign2Categories.id_cam, MVCampaign2Categories.id_cat, unique=True)
